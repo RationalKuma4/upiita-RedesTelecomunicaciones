228e28798b5c8be3d3b096d0814f8df82f36b60c
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2019-05-01 16:36:55 -0500

    Front

---
+++ Front/RtFront/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RtFront
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
